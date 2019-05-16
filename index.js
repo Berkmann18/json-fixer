4e2d84364b300d678deafa3cd81c8a284ddfdade
Berkmann18 <maxieberkmann@gmail.com>
2019-05-16 11:26:09 +0100

    chore: improvements
    Improvements all round and a working _extra-character_ fixer

--- index.js
+++ index.js
@@ -1,32 +1,54 @@
-const {parse, SyntaxError} = require('./src/json')
+const chalk = require('chalk')
+const {parse} = require('./src/json')
 
-const captureError = err => {
-  const errHeader = err.message.match(/^.*?(\d+):/)
-  const errInfo = err.message.match(/Expecting .*? got \S+/)
+/* const captureError = err => {
   return {
-    errHeader,
-    errInfo,
-    errMsg: `${errHeader[0]} ${errInfo}`,
+    msg: err.message,
+    location: err.location,
+    data: err.found,
   }
-}
+} */
 
 const doubleCheck = data => {
   /* eslint-disable no-console */
   try {
     const res = parse(data)
-    console.log('The JSON data was fixed!')
+    process.stdout.write(`\n${chalk.cyan('The JSON data was fixed!')}\n`)
     if (res) return res
   } catch (err) {
     console.error(`There's still an error!`)
-    throw new Error(captureError(err).errMsg)
+    throw new Error(err.message)
   }
   /* eslint-enable no-console */
 }
 
+const removeLinebreak = l => l.replace(/[\n\r]/g, '')
+
+const extraChar = err => {
+  const CLOSING = ['}', ']']
+  return err.expected[0].type === 'other' && CLOSING.includes(err.found)
+}
+
+const trailingDot = err => {
+  return err
+}
+
+const missingChar = err => {
+  return err
+}
+
+/*eslint-disable no-console */
 const fixJson = (err, data) => {
-  const {errHeader, errMsg} = captureError(err)
-  const lnNum = errHeader[1] | 0 // eslint-disable-line no-bitwise
-  console.error(errMsg) // eslint-disable-line no-console
+  const lines = data.split('\n')
+  lines.forEach((l, i) => console.log(i, l))
+  // console.log(chalk.red('err='))
+  // console.dir(err)
+  const start = err.location.start
+  /* const {msg, location} = captureError(err)
+  const lnNum = location.start.line
+  const colNum = location.start.column
+
+  console.error(msg) // eslint-disable-line no-console
   const lines = data.split('\n')
   const brokenLine = lines[lnNum - 1]
   //Removes the character and checks again
@@ -35,8 +57,34 @@ const fixJson = (err, data) => {
   const fixedData = [...lines]
   fixedData[lnNum - 1] = fixedLine
 
-  return doubleCheck(fixedData.join('\n'))
+  return doubleCheck(fixedData.join('\n')) */
+
+  if (extraChar(err)) {
+    //extraChar: s4
+    //Remove char
+    const targetLine = start.line - 2
+    const brokenLine = removeLinebreak(lines[targetLine])
+    // console.log(`broken line='${brokenLine.toString()}'`);
+    let fixedLine = brokenLine.trimEnd()
+    fixedLine = fixedLine.substr(0, fixedLine.length - 1)
+    // console.log(`fixed line='${fixedLine}'`)
+    const fixedData = [...lines]
+    fixedData[targetLine] = fixedLine
+    return doubleCheck(fixedData.join('\n'))
+  } else if (trailingDot) {
+    //s3
+    console.log(chalk.yellow('Fix not supported yet'))
+  } else if (missingChar) {
+    //s5
+    console.log(chalk.yellow('Fix not supported yet'))
+  } else
+    throw new Error(
+      `Unsupported issue: ${err.message} (please open an issue at the repo)`,
+    )
+
+  return null
 }
+/*eslint-enable no-console */
 
 const checkJson = data => {
   //inspired by https://jsontuneup.com/
@@ -49,9 +97,9 @@ const checkJson = data => {
       }
     }
   } catch (err) {
-    console.log('err=', err)
+    // console.log('err=', err)
     return {
-      //data: fixJson(err, data),
+      data: fixJson(err, data),
       changed: true,
     }
   }
--- test/sandbox.js
+++ test/sandbox.js
@@ -1,14 +1,20 @@
-const jsonFix = require('json-fix')
+const fs = require('fs')
+const chalk = require('chalk')
+const jsonFix = require('../')
+const args = process.argv.slice(2)
+const files = args.length ? args : ['s4']
 
 // Get the (potentially malformed) JSON data ready
-const file = './samples/s0.json'
-const jsonContent = fs.readFileSync(file, 'utf-8')
+files.forEach(arg => {
+  const file = `./test/samples/${arg}.json`
+  const jsonContent = fs.readFileSync(file, 'utf-8')
 
-const {data, changed} = jsonFix(jsonContent) // Lint (and fix) it
+  const {data, changed} = jsonFix(jsonContent) // Lint (and fix) it
 
-if (changed) {
-  // Do something with `data` which is the fixed JSON data from `jsonContent`
-  // e.g. `fs.writeFileSync(configPath, JSON.stringify(config, null, 2))`
-  console.log(file, 'changed')
-  console.log(JSON.stringify(data, null, 2))
-} else console.log(file, 'is correct')
+  if (changed) {
+    // Do something with `data` which is the fixed JSON data from `jsonContent`
+    // e.g. `fs.writeFileSync(configPath, JSON.stringify(config, null, 2))`
+    console.log(chalk.underline(`${file} changed:`))
+    console.log(JSON.stringify(data, null, 2))
+  } else console.log(chalk.underline(file, 'is correct'))
+})
