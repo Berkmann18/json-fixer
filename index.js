6e5113b0537a6d9cf4f8a01712306e6a3e387c9b
Berkmann18 <maxieberkmann@gmail.com>
2020-05-26 15:40:09 +0100

    style: formatting

--- index.js
+++ index.js
@@ -1,109 +1,100 @@
-const chalk = require('chalk')
-const {parse} = require('./src/json.pjs')
-const {psw, removeLinebreak} = require('./src/utils')
-const fixer = require('./src/fixer')
+const chalk = require('chalk');
+const { parse } = require('./src/json.pjs');
+const { psw, removeLinebreak } = require('./src/utils');
+const fixer = require('./src/fixer');
 
-let fixRounds = 0
-let roundThreshold = 20
+let fixRounds = 0;
+let roundThreshold = 20;
 
-const setFixThreshold = data => {
-  const lineCount = data.split('\n').length
-  roundThreshold = Math.max(data.length / lineCount, lineCount)
-}
+const setFixThreshold = (data) => {
+  const lineCount = data.split('\n').length;
+  roundThreshold = Math.max(data.length / lineCount, lineCount);
+};
 
 const doubleCheck = (data, options = {}) => {
   /* eslint-disable no-console */
-  const verbose = options.verbose
+  const verbose = options.verbose;
   try {
-    const res = parse(data)
-    psw(`\n${chalk.cyan('The JSON data was fixed!')}`)
+    const res = parse(data);
+    psw(`\n${chalk.cyan('The JSON data was fixed!')}`);
     if (res) {
-      return options.parse ? res : data
+      return options.parse ? res : data;
     }
   } catch (err) {
     if (verbose) {
-      psw('Nearly fixed data:')
-      data.split('\n').forEach((l, i) => psw(`${chalk.yellow(i)} ${l}`))
+      psw('Nearly fixed data:');
+      data.split('\n').forEach((l, i) => psw(`${chalk.yellow(i)} ${l}`));
     }
     // eslint-disable-next-line no-use-before-define
-    if (fixRounds < roundThreshold) return fixJson(err, data, options)
-    console.error(chalk.red(`There's still an error!`))
-    throw new Error(err.message)
+    if (fixRounds < roundThreshold) return fixJson(err, data, options);
+    console.error(chalk.red(`There's still an error!`));
+    throw new Error(err.message);
   }
   /* eslint-enable no-console */
-}
+};
 
-const extraChar = err =>
-  err.expected[0].type === 'other' && ['}', ']'].includes(err.found)
+const extraChar = (err) => err.expected[0].type === 'other' && ['}', ']'].includes(err.found);
 
-const trailingChar = err => {
-  const literal =
-    err.expected[0].type === 'literal' && err.expected[0].text !== ':'
+const trailingChar = (err) => {
+  const literal = err.expected[0].type === 'literal' && err.expected[0].text !== ':';
   return (
-    ['.', ',', 'x', 'b', 'o'].includes(err.found) &&
-    (err.expected[0].type === 'other' || literal)
-  )
-}
+    ['.', ',', 'x', 'b', 'o'].includes(err.found) && (err.expected[0].type === 'other' || literal)
+  );
+};
 
-const missingChar = err =>
-  err.expected[0].text === ',' && ['"', '[', '{'].includes(err.found)
+const missingChar = (err) => err.expected[0].text === ',' && ['"', '[', '{'].includes(err.found);
 
-const singleQuotes = err => err.found === "'"
+const singleQuotes = (err) => err.found === "'";
 
-const missingQuotes = err =>
-  /\w/.test(err.found) && err.expected.find(el => el.description === 'string')
+const missingQuotes = (err) =>
+  /\w/.test(err.found) && err.expected.find((el) => el.description === 'string');
 
-const notSquare = err =>
-  err.found === ':' && [',', ']'].includes(err.expected[0].text)
+const notSquare = (err) => err.found === ':' && [',', ']'].includes(err.expected[0].text);
 
-const notCurly = err => err.found === ',' && err.expected[0].text === ':'
+const notCurly = (err) => err.found === ',' && err.expected[0].text === ':';
 
-const comment = err => err.found === '/'
+const comment = (err) => err.found === '/';
 
-const ops = err =>
-  ['+', '-', '*', '/', '>', '<', '~', '|', '&', '^'].includes(err.found)
+const ops = (err) => ['+', '-', '*', '/', '>', '<', '~', '|', '&', '^'].includes(err.found);
 
 /*eslint-disable no-console */
 const fixJson = (err, data, options) => {
-  ++fixRounds
-  const lines = data.split('\n')
-  const verbose = options.verbose
+  ++fixRounds;
+  const lines = data.split('\n');
+  const verbose = options.verbose;
   if (verbose) {
-    psw(`Data:`)
-    lines.forEach((l, i) => psw(`${chalk.yellow(i)} ${l}`))
-    psw(chalk.red('err='))
-    console.dir(err)
+    psw(`Data:`);
+    lines.forEach((l, i) => psw(`${chalk.yellow(i)} ${l}`));
+    psw(chalk.red('err='));
+    console.dir(err);
   }
-  const start = err.location.start
-  let fixedData = [...lines]
-  const targetLine = start.line - 2
+  const start = err.location.start;
+  let fixedData = [...lines];
+  const targetLine = start.line - 2;
 
   if (extraChar(err)) {
-    fixedData = fixer.fixExtraChar({fixedData, verbose, targetLine})
+    fixedData = fixer.fixExtraChar({ fixedData, verbose, targetLine });
   } else if (trailingChar(err)) {
-    fixedData = fixer.fixTrailingChar({start, fixedData, verbose})
+    fixedData = fixer.fixTrailingChar({ start, fixedData, verbose });
   } else if (missingChar(err)) {
-    if (verbose) psw(chalk.magenta('Missing character'))
-    const brokenLine = removeLinebreak(lines[targetLine])
-    fixedData[targetLine] = `${brokenLine},`
+    if (verbose) psw(chalk.magenta('Missing character'));
+    const brokenLine = removeLinebreak(lines[targetLine]);
+    fixedData[targetLine] = `${brokenLine},`;
   } else if (singleQuotes(err)) {
-    fixedData = fixer.fixSingleQuotes({start, fixedData, verbose})
+    fixedData = fixer.fixSingleQuotes({ start, fixedData, verbose });
   } else if (missingQuotes(err)) {
-    fixedData = fixer.fixMissingQuotes({start, fixedData, verbose})
+    fixedData = fixer.fixMissingQuotes({ start, fixedData, verbose });
   } else if (notSquare(err)) {
-    fixedData = fixer.fixSquareBrackets({start, fixedData, verbose, targetLine})
+    fixedData = fixer.fixSquareBrackets({ start, fixedData, verbose, targetLine });
   } else if (notCurly(err)) {
-    fixedData = fixer.fixCurlyBrackets({fixedData, verbose, targetLine})
+    fixedData = fixer.fixCurlyBrackets({ fixedData, verbose, targetLine });
   } else if (comment(err)) {
-    fixedData = fixer.fixComment({start, fixedData, verbose})
+    fixedData = fixer.fixComment({ start, fixedData, verbose });
   } else if (ops(err)) {
-    fixedData = fixer.fixOpConcat({start, fixedData, verbose})
-  } else
-    throw new Error(
-      `Unsupported issue: ${err.message} (please open an issue at the repo)`,
-    )
-  return doubleCheck(fixedData.join('\n'), options)
-}
+    fixedData = fixer.fixOpConcat({ start, fixedData, verbose });
+  } else throw new Error(`Unsupported issue: ${err.message} (please open an issue at the repo)`);
+  return doubleCheck(fixedData.join('\n'), options);
+};
 /*eslint-enable no-console */
 
 /**
@@ -113,34 +104,34 @@ const fixJson = (err, data, options) => {
  */
 const checkJson = (data, options) => {
   //inspired by https://jsontuneup.com/
-  let optionsCopy
+  let optionsCopy;
   if (!options || typeof options === 'boolean') {
-    optionsCopy = {}
-    optionsCopy.verbose = options
+    optionsCopy = {};
+    optionsCopy.verbose = options;
   } else {
-    optionsCopy = JSON.parse(JSON.stringify(options))
+    optionsCopy = JSON.parse(JSON.stringify(options));
   }
 
   if (optionsCopy.parse === undefined || optionsCopy.parse === null) {
-    optionsCopy.parse = true
+    optionsCopy.parse = true;
   }
 
   try {
-    const res = parse(data)
+    const res = parse(data);
     if (res) {
       return {
         data: optionsCopy.parse ? res : data,
-        changed: false,
-      }
+        changed: false
+      };
     }
   } catch (err) {
-    fixRounds = 0
-    setFixThreshold(data)
+    fixRounds = 0;
+    setFixThreshold(data);
     return {
       data: fixJson(err, data, optionsCopy),
-      changed: true,
-    }
+      changed: true
+    };
   }
-}
+};
 
-module.exports = checkJson
+module.exports = checkJson;
